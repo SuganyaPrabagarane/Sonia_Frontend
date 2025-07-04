c772753035d26bb3ae2bd141bf28223ae4bd73cf
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-07-04 12:47:16 +0300

    import axios in add employee component

--- src/pages/AddEmployee/AddEmployee.jsx
+++ src/pages/AddEmployee/AddEmployee.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import styles from './AddEmployee.module.css';
+import axios from 'axios';
 
 const AddEmployee = ({ onAddEmployee }) => {
   const [formData, setFormData] = useState({
